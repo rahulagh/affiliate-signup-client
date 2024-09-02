f187d91f8f5aeabd4fea09833fe3b64c5eab9d7a
Rahul kumar <rahulkrgupta18032003@gmail.com>
2024-09-02 15:57:08 +0530

    ticket_107_client

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -61,7 +61,7 @@ const SignUpForm = ({ onSubmitSuccess }) => {
         throw new Error('Please select a valid document to upload');
       }
   
-      const response = await fetch('https://affiliate-signup-server.onrender.com', {
+      const response = await fetch('http://localhost:5000/api/users/register', {
         method: 'POST',
         body: formData,
       });
